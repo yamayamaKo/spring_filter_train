4a994169f9306dc1841defe724ffa0b667da3f84
yamayamaKo <yamachanwow@gmail.com>
2025-05-31 01:15:24 +0900

    first commit

---
+++ src/main/kotlin/com/example/filter_train/filter_train/FilterTrainApplication.kt
@@ -0,0 +1,11 @@
+package com.example.filter_train.filter_train
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilterTrainApplication
+
+fun main(args: Array<String>) {
+	runApplication<FilterTrainApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/filter_train/filter_train/FilterTrainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.filter_train.filter_train
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilterTrainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
